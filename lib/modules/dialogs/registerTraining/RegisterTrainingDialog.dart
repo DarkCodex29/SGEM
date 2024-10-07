aba967f7c23ceed109af1e4b80dcb06e4f31d284
Carlos Pineda <52181972+tamashirotod@users.noreply.github.com>
2024-10-07 15:03:46 -0500

    Update RegisterTrainingDialog.dart

--- lib/modules/dialogs/registerTraining/RegisterTrainingDialog.dart
+++ lib/modules/dialogs/registerTraining/RegisterTrainingDialog.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:sgem/modules/dialogs/registerTraining/RegisterTrainingController.dart';
-import 'package:sgem/modules/dialogs/registerTraining/custom.popup.newEntrenamiento.dart';
-import 'package:sgem/modules/pages/personal.training/training/training.personal.controller.dart';
 import 'package:sgem/shared/modules/maestro.detail.dart';
 import 'package:sgem/shared/modules/personal.dart';
 import 'package:sgem/shared/modules/registrar.training.dart';
@@ -12,97 +10,88 @@ import 'package:sgem/shared/widgets/custom.dropdown.dart';
 import 'package:sgem/shared/widgets/custom.textfield.dart';
 
 class RegisterTrainingDialog extends StatelessWidget {
-
   final Personal data;
-  final RegisterTrainingController controller = Get.put(RegisterTrainingController());
-  double paddingVertical = 60;
-  VoidCallback close;
+  final RegisterTrainingController controller =
+      Get.put(RegisterTrainingController());
+  final double paddingVertical = 60;
+  final VoidCallback close;
 
   RegisterTrainingDialog({super.key, required this.data, required this.close});
 
   Widget content(BuildContext context) {
     return Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Expanded( child: 
-                    CustomGenericDropdown<MaestroDetalle>(
-                      hintText: "Equipo",
-                      options: controller.equipoDetalle,
-                      
-                      selectedValue: controller.equipoSelectedBinding,
-                      isSearchable: false,
-                      isRequired: true,
-                    )
-                  ),
-                  SizedBox(width: paddingVertical),
-                  Expanded(child: 
-                  customTextFieldDate("Fecha de inicio dd/mm/yyy", controller.fechaInicioEntrenamiento, true, false, context)
-                  )
-                ],
-              ).padding(EdgeInsets.only(left: paddingVertical, right: paddingVertical)),
-
-              Row(
-                children: [
-                  Expanded( child: 
-                    CustomGenericDropdown<MaestroDetalle>(
-                      hintText: "Condicion",
-                      options: controller.condicionDetalleList,
-                      selectedValue: controller.condicionSelectedBinding,
-                      isSearchable: false,
-                      isRequired: true,
-                    )
-                  ),
-                  SizedBox(width: paddingVertical),
-                  Expanded(child: 
-                    customTextFieldDate("Fecha de termino dd/mm/yyy",controller.fechaTerminoEntrenamiento, true, false, context)
-                  )
-                ],
-              ).padding(EdgeInsets.only(left: paddingVertical, right: paddingVertical)),
-              adjuntarArchivoText().padding(const EdgeInsets.only(bottom: 10)),
-              adjuntarDocumentoPDF(controller),
-              customButtonsCancelAndAcept(
-                () => 
-                  close()
-                , 
-                () =>  registertraining()
-              )
-            ],
-          ).padding(const EdgeInsets.only(top: 10, bottom: 10, left: 30, right: 30));
-        
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Row(
+          children: [
+            Expanded(
+                child: CustomGenericDropdown<MaestroDetalle>(
+              hintText: "Equipo",
+              options: controller.equipoDetalle,
+              selectedValue: controller.equipoSelectedBinding,
+              isSearchable: false,
+              isRequired: true,
+            )),
+            SizedBox(width: paddingVertical),
+            Expanded(
+                child: customTextFieldDate("Fecha de inicio dd/mm/yyy",
+                    controller.fechaInicioEntrenamiento, true, false, context))
+          ],
+        ).padding(
+            EdgeInsets.only(left: paddingVertical, right: paddingVertical)),
+        Row(
+          children: [
+            Expanded(
+                child: CustomGenericDropdown<MaestroDetalle>(
+              hintText: "Condicion",
+              options: controller.condicionDetalleList,
+              selectedValue: controller.condicionSelectedBinding,
+              isSearchable: false,
+              isRequired: true,
+            )),
+            SizedBox(width: paddingVertical),
+            Expanded(
+                child: customTextFieldDate("Fecha de termino dd/mm/yyy",
+                    controller.fechaTerminoEntrenamiento, true, false, context))
+          ],
+        ).padding(
+            EdgeInsets.only(left: paddingVertical, right: paddingVertical)),
+        adjuntarArchivoText().padding(const EdgeInsets.only(bottom: 10)),
+        adjuntarDocumentoPDF(controller),
+        customButtonsCancelAndAcept(() => close(), () => registertraining())
+      ],
+    ).padding(const EdgeInsets.only(top: 10, bottom: 10, left: 30, right: 30));
   }
+
   @override
   Widget build(BuildContext context) {
-
     return Obx(() {
-      return Scaffold( 
-        appBar: AppBar(title: const Text ("Nuevo Entrenamiento")),
-        body: 
-           (controller.isLoading.value) ? 
-        const Center(child: CircularProgressIndicator())
-      : content(context)
-        
-      ).size(400, null);
+      return Scaffold(
+              appBar: AppBar(title: const Text("Nuevo Entrenamiento")),
+              body: (controller.isLoading.value)
+                  ? const Center(child: CircularProgressIndicator())
+                  : content(context))
+          .size(400, null);
     });
   }
 
   void registertraining() {
-    if (controller.equipoSelected.value != null && controller.condicionSelected.value != null) {
+    if (controller.equipoSelected.value != null &&
+        controller.condicionSelected.value != null) {
       controller.registertraining(
-        RegisterTraining(
-          inTipoActividad: 1, 
-          inPersona: data.key,
-          inEquipo: controller.equipoSelected.value!.key, 
-          inCondicion: controller.condicionSelected.value!.key, 
-          fechaInicio: controller.transformDate(controller.fechaInicioEntrenamiento.text),
-          fechaTermino: controller.transformDate(controller.fechaTerminoEntrenamiento.text),
-        ), (isSucces) { 
-          if(isSucces != null) {
-            close();
-          } else {
-
-          }
+          RegisterTraining(
+            inTipoActividad: 1,
+            inPersona: data.key,
+            inEquipo: controller.equipoSelected.value!.key,
+            inCondicion: controller.condicionSelected.value!.key,
+            fechaInicio: controller
+                .transformDate(controller.fechaInicioEntrenamiento.text),
+            fechaTermino: controller
+                .transformDate(controller.fechaTerminoEntrenamiento.text),
+          ), (isSucces) {
+        if (isSucces != null) {
+          close();
+        } else {}
       });
     }
   }
@@ -123,41 +112,37 @@ class RegisterTrainingDialog extends StatelessWidget {
     );
   }
 
-
-  Widget customButtonsCancelAndAcept(VoidCallback onCancel, VoidCallback onSave) {
-    return  
-      Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          OutlinedButton(
-            onPressed: onCancel,
-            style: OutlinedButton.styleFrom(
-              side: const BorderSide(color: Colors.grey),
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-            ),
-            child: const Text(
-              'Cerrar',
-              style: TextStyle(color: Colors.grey),
-            ),
-          ),
-          const SizedBox(width: 16),
-          ElevatedButton(
-            onPressed: onSave,
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.blue,
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
-            ),
-            child: const Text(
-              'Guardar',
-              style: TextStyle(color: Colors.white),
-            ),
-          ),
-        ]
-      );
-    }
+  Widget customButtonsCancelAndAcept(
+      VoidCallback onCancel, VoidCallback onSave) {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      OutlinedButton(
+        onPressed: onCancel,
+        style: OutlinedButton.styleFrom(
+          side: const BorderSide(color: Colors.grey),
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        ),
+        child: const Text(
+          'Cerrar',
+          style: TextStyle(color: Colors.grey),
+        ),
+      ),
+      const SizedBox(width: 16),
+      ElevatedButton(
+        onPressed: onSave,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.blue,
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+        ),
+        child: const Text(
+          'Guardar',
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    ]);
   }
+}
 
-  Widget adjuntarDocumentoPDF(RegisterTrainingController controller) {
+Widget adjuntarDocumentoPDF(RegisterTrainingController controller) {
   return Obx(() {
     if (controller.documentoAdjuntoNombre.value.isNotEmpty) {
       return Row(
@@ -191,18 +176,22 @@ class RegisterTrainingDialog extends StatelessWidget {
   });
 }
 
-Widget customTextFieldDate(String label, TextEditingController fechaIngresoMinaController, bool isEditing, bool isViewing, BuildContext context) {
+Widget customTextFieldDate(
+    String label,
+    TextEditingController fechaIngresoMinaController,
+    bool isEditing,
+    bool isViewing,
+    BuildContext context) {
   return Expanded(
     child: CustomTextField(
       label: label,
       controller: fechaIngresoMinaController,
-      icon: const Icon(Icons.calendar_today) ,
+      icon: const Icon(Icons.calendar_today),
       isReadOnly: isViewing,
       isRequired: !isViewing,
       onIconPressed: () {
         if (!isViewing) {
-          _selectDate(
-              context, fechaIngresoMinaController);
+          _selectDate(context, fechaIngresoMinaController);
         }
       },
     ),
@@ -210,16 +199,14 @@ Widget customTextFieldDate(String label, TextEditingController fechaIngresoMinaC
 }
 
 Future<void> _selectDate(
-      BuildContext context, TextEditingController controller) async {
-    DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2100),
-    );
-    if (picked != null) {
-      controller.text = DateFormat('yyyy-MM-dd').format(picked);
-    }
-    
+    BuildContext context, TextEditingController controller) async {
+  DateTime? picked = await showDatePicker(
+    context: context,
+    initialDate: DateTime.now(),
+    firstDate: DateTime(2000),
+    lastDate: DateTime(2100),
+  );
+  if (picked != null) {
+    controller.text = DateFormat('yyyy-MM-dd').format(picked);
+  }
 }
-  

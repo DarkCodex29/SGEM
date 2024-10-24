7793c60453d41bbd9b34e6d21035d5f45527f9c4
Carlos Pineda <52181972+tamashirotod@users.noreply.github.com>
2024-10-24 18:05:07 -0500

    update

--- lib/modules/pages/capacitaciones/actualizacion.masiva/capacitacion.carga.masiva.controller.dart
+++ lib/modules/pages/capacitaciones/actualizacion.masiva/capacitacion.carga.masiva.controller.dart
@@ -1,5 +1,47 @@
+import 'dart:developer';
+import 'dart:typed_data';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class CapacitacionCargaMasivaController extends GetxController{
+TextEditingController archivoController = TextEditingController();
 
+var actualizacionMasivaResultados= [].obs;
+var rowsPerPage = 10.obs;
+var currentPage = 1.obs;
+var totalPages = 1.obs;
+var totalRecords = 0.obs;
+
+Future<void> cargarArchivo() async {
+  try {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      allowMultiple: true,
+      type: FileType.custom,
+      allowedExtensions: ['xlsx'],
+    );
+
+    if (result != null) {
+      for (var file in result.files) {
+        if (file.bytes != null) {
+          Uint8List fileBytes = file.bytes!;
+          String fileName = file.name;
+
+          // archivosAdjuntos.add({
+          //   'nombre': fileName,
+          //   'bytes': fileBytes,
+          //   'nuevo': true,
+          // });
+          archivoController.text= fileName;
+          log('Documento adjuntado correctamente: $fileName');
+        }
+      }
+    } else {
+      log('No se seleccionaron archivos');
+    }
+  } catch (e) {
+    log('Error al adjuntar documentos: $e');
+  }
+}
 }
--- lib/modules/pages/capacitaciones/actualizacion.masiva/capacitacion.carga.masiva.page.dart
+++ lib/modules/pages/capacitaciones/actualizacion.masiva/capacitacion.carga.masiva.page.dart
@@ -1,8 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sgem/modules/pages/capacitaciones/actualizacion.masiva/capacitacion.carga.masiva.controller.dart';
+import 'package:sgem/shared/widgets/custom.textfield.dart';
 
 import '../../../../config/theme/app_theme.dart';
+import '../../../../shared/widgets/dynamic.table/dynamic.table.cabecera.dart';
 
 class CapacitacionCargaMasivaPage extends StatelessWidget {
   const CapacitacionCargaMasivaPage({super.key, required this.onCancel});
@@ -10,7 +13,8 @@ class CapacitacionCargaMasivaPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final CapacitacionCargaMasivaController controller= Get.put(CapacitacionCargaMasivaController());
+    final CapacitacionCargaMasivaController controller =
+        Get.put(CapacitacionCargaMasivaController());
     return Scaffold(
       body: _buildCapacitacionCargaMasivaPage(
         controller,
@@ -18,6 +22,7 @@ class CapacitacionCargaMasivaPage extends StatelessWidget {
       ),
     );
   }
+
   Widget _buildCapacitacionCargaMasivaPage(
       CapacitacionCargaMasivaController controller, BuildContext context) {
     return LayoutBuilder(
@@ -30,7 +35,7 @@ class CapacitacionCargaMasivaPage extends StatelessWidget {
               const SizedBox(
                 height: 20,
               ),
-              //_buildSeccionResultado(controller),
+              _buildSeccionResultado(controller),
               const SizedBox(
                 height: 20,
               ),
@@ -41,6 +46,153 @@ class CapacitacionCargaMasivaPage extends StatelessWidget {
       },
     );
   }
+
+  Widget _buildSeccionResultado(CapacitacionCargaMasivaController controller) {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(12),
+        border: Border.all(color: Colors.grey.shade300),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          _buildSeccionResultadoBarraSuperior(controller),
+          const SizedBox(
+            height: 20,
+          ),
+          _buildSeccionResultadoTabla(controller),
+          const SizedBox(
+            height: 20,
+          ),
+          //_buildSeccionResultadoTablaPaginado(controller),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSeccionResultadoBarraSuperior(
+      CapacitacionCargaMasivaController controller) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        IconButton(
+          onPressed: () {
+            controller.cargarArchivo();
+          },
+          icon: const Icon(
+            Icons.attach_file,
+            color: Colors.blue,
+            size: 32,
+          ),
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        Expanded(
+            child: CustomTextField(
+          label: "Archivo",
+          controller: controller.archivoController,
+        )),
+        const SizedBox(
+          width: 20,
+        ),
+        Row(
+          children: _buildBotonesAccion(controller),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSeccionResultadoTabla(
+      CapacitacionCargaMasivaController controller) {
+    List<String> cabecera = [
+      'Código MCP',
+      'DNI',
+      'Nombres y apellidos',
+      'Guardia',
+      'Entrenador responsable',
+      'Nombre capacitación',
+      'Categoría',
+      'Empresa de capacitación',
+      'Fecha de inicio',
+      'Fecha de término',
+      'Horas',
+      'Nota teórica',
+      'Nota práctica',
+    ];
+    // if (controller.entrenamientoResultados.isEmpty) {
+    //   return const Center(child: Text("No se encontraron resultados"));
+    // }
+
+    return Obx(
+      () {
+         var rowsToShow = controller.actualizacionMasivaResultados
+             .take(controller.rowsPerPage.value)
+             .toList();
+
+        return Column(
+          children: [
+            DynamicTableCabecera(cabecera: cabecera),
+            //_buildSeccionResultadoTablaData(rowsToShow, controller),
+          ],
+        );
+      },
+    );
+  }
+
+  List<Widget> _buildBotonesAccion(
+      CapacitacionCargaMasivaController controller) {
+    return [
+      ElevatedButton.icon(
+        onPressed: () {
+          //controller.showActualizacionMasiva();
+        },
+        icon: const Icon(
+          Icons.upload_rounded,
+          size: 18,
+          color: AppTheme.infoColor,
+        ),
+        label: const Text(
+          "Previsualizar carga",
+          style: TextStyle(fontSize: 16, color: AppTheme.infoColor),
+        ),
+        style: ElevatedButton.styleFrom(
+          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+          backgroundColor: AppTheme.primaryColor,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+            side: const BorderSide(color: AppTheme.primaryColor),
+          ),
+        ),
+      ),
+      const SizedBox(width: 10),
+      ElevatedButton.icon(
+        onPressed: () {
+          //controller.showNewPersonal();
+        },
+        icon: const Icon(
+          Icons.download_rounded,
+          size: 18,
+          color: AppTheme.primaryBackground,
+        ),
+        label: const Text(
+          "Descargar plantilla",
+          style: TextStyle(fontSize: 16, color: AppTheme.primaryBackground),
+        ),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppTheme.primaryColor,
+          padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+            side: const BorderSide(color: AppTheme.primaryColor),
+          ),
+        ),
+      ),
+    ];
+  }
+
   Widget _buildRegresarButton(BuildContext context) {
     return Center(
       child: ElevatedButton(

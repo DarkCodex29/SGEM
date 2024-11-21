10d40a5943fedbf7506b57c63d95e0a7f82536fe
LUSEDOU <dolorier_uribe@hotmail.com>
2024-11-21 10:28:58 -0500

    fix api modulo maestro

--- lib/config/api/api.modulo.maestro.dart
+++ lib/config/api/api.modulo.maestro.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:developer';
 import 'package:dio/dio.dart';
+import 'package:logging/logging.dart';
 import 'package:sgem/config/api/response.handler.dart';
 import 'package:sgem/config/constants/config.dart';
 import 'package:sgem/shared/modules/entrenamiento.modulo.dart';
@@ -203,7 +204,7 @@ class ModuloMaestroService {
     Modulo data,
   ) async {
     try {
-      await _dio.post<dynamic>(
+      await _dio.put<dynamic>(
         '/ActualizarModuloMaestro',
         data: data.toJson(),
       );
